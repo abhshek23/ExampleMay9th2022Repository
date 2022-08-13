56b73523af5d915679114a98457d711877c3feb9
abhshek23 <abhishekkmd1@gmail.com>
2022-08-13 16:48:11 +0530

    BDD Automation

---
+++ BDDCucumberAutomation/src/test/java/com/sgtesting/actitime/testsuit/DriverScript.java
@@ -0,0 +1,45 @@
+package com.sgtesting.actitime.testsuit;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+
+@CucumberOptions(
+		features="src/test/resource/Featurefile/ModifyUser.feature",
+		glue={"com.sgtesting.actitime.stepdefinition"}
+		)
+public class DriverScript 
+{
+	
+	TestNGCucumberRunner testNGCucumberRunner;
+
+	@BeforeClass(alwaysRun = true)
+	public void setUpClass() throws Exception {
+		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	}
+
+	@Test(dataProvider = "features")
+	public void feature(CucumberFeatureWrapper cucumberFeature) {
+		testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+	}
+
+	@DataProvider
+	public Object[][] features() {
+
+		if(testNGCucumberRunner == null){
+			testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+		}
+		return testNGCucumberRunner.provideFeatures();
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDownClass() throws Exception {
+		testNGCucumberRunner.finish();
+	}
+
+}
